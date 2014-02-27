437723679cc18b41e8a92d8798a988cf116a5c56
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-02-27 23:27:51 +0900

    accepts --config options only

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	. "github.com/fujiwara/zabbix-aggregate-agent/zabbix_aggregate_agent"
+	zaa "./zabbix_aggregate_agent"
 	"flag"
 	"log"
 )
 
-func runByConfig(config string) {
-	agents, err := NewAgentsFromConfig(config)
+func main() {
+	var config string
+	flag.StringVar(&config, "config", "", "config file")
+	flag.Parse()
+	agents, err := zaa.NewAgentsFromConfig(config)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -19,48 +22,4 @@ func runByConfig(config string) {
 		<-ch
 	}
 	log.Fatalln("All of agents could not be run.")
-}
-
-func main() {
-	var (
-		listen      string
-		listFile    string
-		listArg     string
-		listCommand string
-		timeout     int
-		expires     int
-		config      string
-	)
-	flag.StringVar(&listen, "listen", DefaultAddress, "listen address e.g. 0.0.0.0:10052")
-	flag.StringVar(&listFile, "list-file", "", "zabbix-agent list file")
-	flag.StringVar(&listCommand, "list-command", "", "command which prints zabbix-agent list to stdout")
-	flag.StringVar(&listArg, "list", "", "zabbix-agent list , separated. e.g. 'web.example.com:10050,192.168.1.1:10050'")
-	flag.IntVar(&timeout, "timeout", 0, "network timeout with zabbix-agent (seconds)")
-	flag.IntVar(&expires, "expires", 0, "list cache expires (seconds)")
-	flag.StringVar(&config, "config", "", "config file")
-	flag.Parse()
-
-	if config != "" {
-		runByConfig(config)
-		return
-	}
-
-	agent := NewAgent("1", listen, timeout)
-	if listFile != "" {
-		agent.ListGenerator = NewListFromFileGenerator(listFile)
-	} else if listArg != "" {
-		agent.ListGenerator = NewListFromArgGenerator(listArg)
-	} else if listCommand != "" {
-		agent.ListGenerator = NewCachedListGenerator(
-			NewListFromCommandGenerator(listCommand),
-			expires,
-		)
-	} else {
-		log.Fatalln("option either --config, --list, --list-file or --list-command is required.")
-	}
-	err := agent.Run()
-	if err != nil {
-		log.Fatalln("Error", err)
-	}
-	return
 }
