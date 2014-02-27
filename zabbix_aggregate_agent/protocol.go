988d4cdab8af34a52d23ed3b148ffd8e410a3a88
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-02-27 14:35:07 +0900

    compat text packet

--- zabbix_aggregate_agent/protocol.go
+++ zabbix_aggregate_agent/protocol.go
@@ -17,9 +17,12 @@ const (
 	ErrorMessage     = "ZBX_NOTSUPPORTED"
 )
 
+var Terminator = []byte("\n")
+var HeaderBytes = []byte(HeaderString)
+
 func data2Packet(data []byte) []byte {
 	buf := new(bytes.Buffer)
-	buf.WriteString(HeaderString)
+	buf.Write(HeaderBytes)
 	binary.Write(buf, binary.LittleEndian, HeaderVersion)
 	binary.Write(buf, binary.LittleEndian, int64(len(data)))
 	buf.Write(data)
@@ -43,23 +46,26 @@ func packet2Data(packet []byte) (data []byte, err error) {
 
 func stream2Data(conn net.Conn) (rdata []byte, err error) {
 	// read header "ZBXD\x01"
-	headBuf := make([]byte, DataLengthOffset)
-	_, err = conn.Read(headBuf)
+	head := make([]byte, DataLengthOffset)
+	_, err = conn.Read(head)
 	if err != nil {
 		return
 	}
-	if string(headBuf[0:HeaderLength]) != HeaderString || headBuf[HeaderLength] != byte(HeaderVersion) {
-		err = errors.New("invalid header")
-		return
+	if bytes.Equal(head[0:HeaderLength], HeaderBytes) && head[HeaderLength] == byte(HeaderVersion) {
+		rdata, err = parseBinary(conn)
+	} else {
+		rdata, err = parseText(conn, head)
 	}
+	return
+}
 
+func parseBinary(conn net.Conn) (rdata []byte, err error) {
 	// read data length
 	var dataLength int64
 	err = binary.Read(conn, binary.LittleEndian, &dataLength)
 	if err != nil {
 		return
 	}
-
 	// read data body
 	buf := make([]byte, 1024)
 	data := new(bytes.Buffer)
@@ -75,6 +81,35 @@ func stream2Data(conn net.Conn) (rdata []byte, err error) {
 		}
 		total = total + size
 		data.Write(buf[0:size])
+	}
+	rdata = data.Bytes()
+	return
+}
+
+func parseText(conn net.Conn, head []byte) (rdata []byte, err error) {
+	data := new(bytes.Buffer)
+	data.Write(head)
+	buf := make([]byte, 1024)
+	size := 0
+	for {
+		// read data while "\n" found
+		size, err = conn.Read(buf)
+		if err != nil {
+			return
+		}
+		if size == 0 {
+			break
+		}
+		i := bytes.Index(buf[0:size], Terminator)
+		if i == -1 {
+			// terminator not found
+			data.Write(buf[0:size])
+			continue
+		} else {
+			// terminator found
+			data.Write(buf[0:i])
+			break
+		}
 	}
 	rdata = data.Bytes()
 	return
