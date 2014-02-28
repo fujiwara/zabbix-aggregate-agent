1e98627bf953576aeb254201ae1cba14bf38366e
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-02-28 16:36:31 +0900

    fill a default port number to host address

--- zabbix_aggregate_agent/client.go
+++ zabbix_aggregate_agent/client.go
@@ -2,10 +2,25 @@ package zabbix_aggregate_agent
 
 import (
 	"net"
+	"strings"
 	"time"
 )
 
+func IsIncludesPortNumber (addr string) bool {
+	if strings.Index(addr, "[") == 0 && strings.Index(addr, "]") == len(addr) - 1 {
+		// ipv6
+		return false
+	}
+	if strings.Index(addr, ":") == -1 {
+		return false
+	}
+	return true
+}
+
 func Get(host string, key string, timeout int) (value []byte, err error) {
+	if ! IsIncludesPortNumber(host) {
+		host = host + ":10050"
+	}
 	conn, err := net.DialTimeout("tcp", host, time.Duration(timeout)*time.Second)
 	if err != nil {
 		return
---
+++ zabbix_aggregate_agent/client_test.go
@@ -0,0 +1,34 @@
+package zabbix_aggregate_agent_test
+
+import (
+	. "../zabbix_aggregate_agent"
+	"testing"
+)
+
+func TestIncludesPortNumber(t *testing.T) {
+	var addrWithPort = []string{
+		"example.com:10050",
+		"192.0.2.1:10050",
+		"[2001:db8::dead:beef]:10050",
+	}
+	for _, addr := range addrWithPort {
+		if IsIncludesPortNumber(addr) {
+			// ok
+		} else {
+			t.Errorf("%s includes port number", addr)
+		}
+	}
+}
+
+func TestNotIncludesPortNumber(t *testing.T) {
+	var addrWithoutPort = []string{
+		"example.com",
+		"192.0.2.1",
+		"[2001:db8::dead:beef]",
+	}
+	for _, addr := range addrWithoutPort {
+		if IsIncludesPortNumber(addr) {
+			t.Errorf("%s not includes port number", addr)
+		}
+	}
+}
